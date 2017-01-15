a97e290c06acc08c7ca3231166005aec1e98281d
Gabor Till <gabor.till@kpn.com>
2017-01-15 18:39:28 +0100

    build(webpack): generate index.html and bundle.js

---
+++ config/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const attainment = {};
+attainment.srcDir = path.resolve(__dirname, '../src');
+attainment.distDir = path.resolve(__dirname, '../dist');
+
+attainment.entry = path.join(attainment.srcDir, 'app/root.module.js');
+attainment.index = path.join(attainment.srcDir, 'index.html');
+
+const loaders = {
+    index: {
+        include: attainment.srcDir,
+        loader: 'html-loader',
+        test: /^index\.html$/,
+    },
+};
+
+const plugins = {
+    index: new HtmlWebpackPlugin({
+        template: attainment.index,
+    }),
+};
+
+module.exports = {
+    devServer: {
+        outputPath: attainment.distDir,
+        port: 8080,
+        stats: 'errors-only',
+    },
+    devtool: 'source-map',
+    entry: attainment.entry,
+    module: {
+        loaders: [
+            loaders.index,
+        ],
+    },
+    output: {
+        filename: 'js/bundle.[hash].js',
+        path: attainment.distDir,
+    },
+    plugins: [
+        plugins.index,
+    ],
+};
